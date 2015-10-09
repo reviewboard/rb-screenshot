318d1550d78f24cc70c0cefe4aaf20c9ec4a50a9
Justin Wu <justin.wu@animaindustries.com>
2015-10-08 21:06:19 -0600

    Add chrome menu when button pressed.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp');
+var less = require('gulp-less')
 
 gulp.task('default', function() {
 	console.log("I have configured a gulpfile");
 });
 
-gulp.task('build', ['chrome', 'chrome-icons', 'firefox',
-					'firefox-icons'], function() {
+gulp.task('build', ['chrome', 'icons', 'chrome-html',
+					'firefox', 'css'], function() {
 	console.log("Building.");
 });
 
@@ -14,17 +15,24 @@ gulp.task('chrome', function() {
 		.pipe(gulp.dest(".build/chrome"));
 });
 
-gulp.task('chrome-icons', function() {
+gulp.task('icons', function() {
 	return gulp.src("content/icons/*")
-		.pipe(gulp.dest(".build/chrome/icons"));
+		.pipe(gulp.dest(".build/chrome/icons"))
+		.pipe(gulp.dest(".build/firefox/data"));
+});
+
+gulp.task('css', function() {
+	return gulp.src("content/css/*.less")
+		.pipe(less())
+		.pipe(gulp.dest(".build/chrome/css"));
+});
+
+gulp.task('chrome-html', function() {
+	return gulp.src("content/*.html")
+		.pipe(gulp.dest(".build/chrome"));
 });
 
 gulp.task('firefox', function() {
 	return gulp.src("xpi/*")
 		.pipe(gulp.dest(".build/firefox"));
-});
-
-gulp.task("firefox-icons", function() {
-	return gulp.src("content/icons/*")
-		.pipe(gulp.dest(".build/firefox/data"));
 });
