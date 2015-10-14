dac2361ff755ad925dfd5816c6dfbe496ca33fd6
Justin Wu <justin.wu@animaindustries.com>
2015-10-13 18:25:21 -0600

    Can retrieve all of a user's review requests on local server

--- content/js/screenshot.js
+++ content/js/screenshot.js
@@ -2,7 +2,7 @@ function setScreenshotUrl(url) {
     document.getElementById('screenshot').src = url;
 }
 
-function createRequest() {
+function createXMLRequest() {
 	var result = null;
 	if (window.XMLHttpRequest) {
 		result = new XMLHttpRequest();
@@ -19,19 +19,27 @@ function createRequest() {
 // CURRENT CODE IS FOR TESTING ON MY LOCALSERVER
 // WILL NOT WORK ON OTHER SERVERS YET
 function getUserReviewRequests() {
-	var req = createRequest();
-	req.open('get', 'http://localhost:8080/api/review-requests/?from-user=qyfcb', true);
-	req.onreadystatechange = function() {
-		if (req.readyState != 4) return;
-		if (req.status != 200) {
+	var xml_req = createXMLRequest();
+	xml_req.open('get', 'http://localhost:8080/api/review-requests/?from-user=qyfcb',
+				 true);
+	xml_req.onreadystatechange = function() {
+		if (xml_req.readyState != 4) return;
+		if (xml_req.status != 200) {
 			// Request failure handling
 			return;
 		}
-		console.log('shoot');
-		var resp = req.responseText;
-		document.getElementById('request-result-placeholder').innerHTML = resp;
+		var req_info = JSON.parse(xml_req.responseText);
+		var req_count = req_info.total_results;
+		var req_dropdown =  document.getElementById('rr-id-select');
+
+		for (i = 0; i < req_count; i++) {
+			var option = document.createElement('option');
+			option.text = 'r/' + req_info.review_requests[i].id + ' - ' +
+						  req_info.review_requests[i].summary;
+			req_dropdown.add(option);
+		}
 	};
-	req.send();
+	xml_req.send();
 }
 
 window.onload = function() {
