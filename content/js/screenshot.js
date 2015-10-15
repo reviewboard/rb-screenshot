5e0d8136af3ad5492858d5ef2f6f53a470b35385
Justin Wu <justin.wu@animaindustries.com>
2015-10-14 18:41:22 -0600

    Use JQuery for AJAX

--- content/js/screenshot.js
+++ content/js/screenshot.js
@@ -1,47 +1,31 @@
+$(document).ready(function() {
+	reviewRequests();
+});
+
 function setScreenshotUrl(url) {
     document.getElementById('screenshot').src = url;
 }
 
-function createXMLRequest() {
-	var result = null;
-	if (window.XMLHttpRequest) {
-		result = new XMLHttpRequest();
-		if (typeof result.overrideMimeType != 'undefined') {
-			result.overrideMimeType('text/xml');
-		} else if (window.ActiveXObject) {
-			result = new ActiveXObject("Microsoft.XMLHTTP");
-		}
-	}
-
-	return result;
-}
-
 // CURRENT CODE IS FOR TESTING ON MY LOCALSERVER
 // WILL NOT WORK ON OTHER SERVERS YET
-function getUserReviewRequests() {
-	var xml_req = createXMLRequest();
-	xml_req.open('get', 'http://localhost:8080/api/review-requests/?from-user=qyfcb',
-				 true);
-	xml_req.onreadystatechange = function() {
-		if (xml_req.readyState != 4) return;
-		if (xml_req.status != 200) {
-			// Request failure handling
-			return;
-		}
-		var req_info = JSON.parse(xml_req.responseText);
-		var req_count = req_info.total_results;
+function reviewRequests() {
+	$.ajax({
+		url: 'http://localhost:8080/api/review-requests/',
+		type: 'get',
+		data: {
+			'from-user': 'qyfcb'
+		},
+		dataType: 'json',
+		success: function(json) {
+		var req_count = json.total_results;
 		var req_dropdown =  document.getElementById('rr-id-select');
 
 		for (i = 0; i < req_count; i++) {
 			var option = document.createElement('option');
-			option.text = 'r/' + req_info.review_requests[i].id + ' - ' +
-						  req_info.review_requests[i].summary;
+			option.text = 'r/' + json.review_requests[i].id + ' - ' +
+						  json.review_requests[i].summary;
 			req_dropdown.add(option);
 		}
-	};
-	xml_req.send();
+		}
+	});
 }
-
-window.onload = function() {
-	getUserReviewRequests();
-};
