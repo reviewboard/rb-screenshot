a89cc7faae9921e6388e3e5fd17281fe8f531d3a
Justin Wu <justin.wu@animaindustries.com>
2015-10-22 15:27:47 -0600

    Remove useless function. Separate screenshot.html.

--- content/js/add_user.js
+++ content/js/add_user.js
@@ -1,6 +1,4 @@
 // Replaces the submit event on the add_user form.
-// TODO: Firefox needs to have a save_user.js file that will save the form
-// 		 data to the browser's storage via a save_information() function.
 $(document).ready(function() {
 
     $('#submit').click(function(event) {
@@ -11,10 +9,6 @@ $(document).ready(function() {
 
     var dialog;
 	var form;
-
-	function addUser() {
-		console.log('Add User');
-	}
 
 	dialog = $('#dialog-form').dialog({
 		autoOpen: false
