31dac7cdc215a77eefca842351aa03217e08a341
datjwu <justin.h.y.wu@gmail.com>
2015-10-24 14:44:25 -0600

    Move comments to better location and fix spacing

--- content/js/add_user.js
+++ content/js/add_user.js
@@ -1,21 +1,21 @@
-// Replaces the submit event on the add_user form.
 $(document).ready(function() {
 
+    // Replaces the submit event on the add_user form.
     $('#submit').click(function(event) {
         event.preventDefault();
         saveInformation();
         dialog.dialog('close');
     });
 
+    // Set up add user form
     var dialog;
-	var form;
+    var form;
 
-	dialog = $('#dialog-form').dialog({
-		autoOpen: false
-	});
+    dialog = $('#dialog-form').dialog({
+    	autoOpen: false
+    });
 
-	$('#add-user-button').button().on('click', function() {
-		dialog.dialog('open');
-	});
-
+    $('#add-user-button').button().on('click', function() {
+    	dialog.dialog('open');
+    });
 });
