81600562521b75375382e2a8131ff82b5b1f4f61
Justin Wu <justin.h.y.wu@gmail.com>
2015-11-21 13:03:36 -0700

    [Firefox] Crop buttons shown after setting the crop area

--- xpi/js/crop.js
+++ xpi/js/crop.js
@@ -1,3 +1,5 @@
+var jcropAPI;
+
 function createInput(id) {
     var input = document.createElement('input');
     input.type = 'hidden';
@@ -8,25 +10,56 @@ function createInput(id) {
 }
 
 function createCropButton() {
+    var cropButton = document.createElement('img');
+    cropButton.id = 'crop-button';
+    cropButton.className = 'crop-container-button';
+    cropButton.src = 'resource://rb-screenshot-images/crop.png';
+    cropButton.addEventListener('click', function() {
+        console.log('crop clicked');
+    });
+
+    var exitButton = document.createElement('img');
+    exitButton.id = 'exit-button';
+    exitButton.className = 'crop-container-button'
+    exitButton.src = 'resource://rb-screenshot-images/exit.png';
+    exitButton.addEventListener('click', function() {
+        console.log('exit clicked');
+    });
+
+    var divider = document.createElement('div');
+    divider.className = 'vertical-divider';
+
     var div = document.createElement('div');
     div.id = 'crop-button-container';
-    div.innerHTML = '<img src="resource://rb-screenshot-images/crop.png">';
-
+    div.appendChild(cropButton);
+    div.appendChild(divider);
+    div.appendChild(exitButton);
     document.body.appendChild(div);
 }
 
 function createCoordinateForm() {
     var coord_form = document.createElement('form');
+    coord_form.style.display = 'none';
     var x = createInput('x');
     var y = createInput('y');
     var w = createInput('w');
     var h = createInput('h');
+    x.value = 100;
+    y.value = 100;
+    w.value = 50;
+    h.value = 50;
     coord_form.appendChild(x);
     coord_form.appendChild(y);
     coord_form.appendChild(w);
     coord_form.appendChild(h);
 
     document.getElementById('crop-button-container').appendChild(coord_form);
+}
+
+function destroy() {
+    jcropAPI.destroy();
+    $('#crop-button-container').remove();
+    $('#rb-area').remove();
 }
 
 function hideCropButton() {
@@ -43,16 +76,27 @@ function updateCoords(c) {
     $('#w').val(c.w);
     $('#h').val(c.h);
 
-    // 40px is the width of the crop image.
-    $('#crop-button-container').css({
-        'top': c.y + c.h,
-        'left': c.x + c.w - 40
-    });
-
+    updateCropButton(c.x2, c.y2);
     showCropButton();
 }
 
-$('document').ready(function() {
+function updateCropButton(x, y) {
+    // 45px is the width of the crop image.
+    $('#crop-button-container').css({
+        'top': y,
+        'left': x - document.getElementById('crop-button-container').clientWidth
+    });
+}
+
+function resetSelect() {
+    var x = Number($('#x').val());
+    var y = Number($('#y').val());
+    var x2 = x + Number($('#w').val());
+    var y2 = y + Number($('#h').val());
+    jcropAPI.setSelect([x, y, x2, y2]);
+}
+
+$(document).ready(function() {
     var imageOverlay = document.getElementById('rb-image-overlay');
 
     if (imageOverlay) {
@@ -63,13 +107,22 @@ $('document').ready(function() {
             bgOpacity: .4,
             onChange: hideCropButton,
             onSelect: updateCoords,
-            setSelect: [100, 100, 50, 50]
+            onRelease: resetSelect,
+            setSelect: [0, 0, 0, 0]
+        }, function() {
+            jcropAPI = this;
+        });
+
+        $(document).keyup(function(event) {
+            if (event.keyCode == 27) {
+                destroy();
+            }
+        });
+
+        $(document).on('contextmenu', '.jcrop-holder', function(event) {
+            destroy();
         });
 
         createCoordinateForm();
     }
-});
-
-$(window).on('beforeunload', function() {
-    $('#crop-button-container').remove();
 });
